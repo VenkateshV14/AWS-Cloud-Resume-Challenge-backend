cb714b473f79048b8b9b3d1a853cd1f3ebab9059
VenkateshV14 <vakamudilavenkatesh@gmail.com>
2025-03-28 17:15:54 +0530

    cicd-backend

--- api-gateway.tf
+++ api-gateway.tf
@@ -7,6 +7,10 @@ resource "aws_apigatewayv2_route" "visitor_route" {
   api_id    = aws_apigatewayv2_api.visitor_api.id
   route_key = "GET /visitorCounter"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
@@ -27,4 +31,8 @@ resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.visitor_api.id
   name        = "$default"
   auto_deploy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }

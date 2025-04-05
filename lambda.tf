f4d8e19c6fd547045b35361e4736d6ad3a4fde93
VenkateshV14 <vakamudilavenkatesh@gmail.com>
2025-04-05 18:46:27 +0530

    updated new changes

--- lambda.tf
+++ lambda.tf
@@ -6,6 +6,10 @@ resource "aws_lambda_function" "visitor_counter" {
   runtime          = "python3.11"
   role             = aws_iam_role.lambda_exec_role.arn
 
-  timeout = 10
+  timeout     = 10
+  memory_size = 128
+  publish     = true
 }
+
+
 

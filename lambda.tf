92a648bc7e30977d3983663b4e478a9d9a74e4aa
VenkateshV14 <vakamudilavenkatesh@gmail.com>
2025-04-05 17:30:50 +0530

    updated new changes

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "visitor_counter" {
   function_name    = var.lambda_function_name
-  filename         = "lambda_function.zip"
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  filename         = "${path.module}/lambda_function.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.11"
   role             = aws_iam_role.lambda_exec_role.arn
